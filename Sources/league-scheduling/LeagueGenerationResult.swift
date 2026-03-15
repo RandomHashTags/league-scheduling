888eb9b9c21771a7d64eb0662041a82c139357b4
RandomHashTags <imrandomhashtags@gmail.com>
2026-03-15 12:46:11 -0500

    add `Codable` conformance to `LeagueGenerationResult` under the `ProtobufCodable` package trait

--- Sources/league-scheduling/LeagueGenerationResult.swift
+++ Sources/league-scheduling/LeagueGenerationResult.swift
@@ -3,3 +3,8 @@ public struct LeagueGenerationResult: Sendable {
     public let results:[LeagueGenerationData]
     public let error:String?
 }
+
+#if ProtobufCodable
+extension LeagueGenerationResult: Codable {
+}
+#endif

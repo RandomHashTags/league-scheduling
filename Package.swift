6ae650c826fe0f210e778ec9c739528a80d1dc37
RandomHashTags <imrandomhashtags@gmail.com>
2026-03-16 07:13:55 -0500

    resolving merge conflicts

--- Package.swift
+++ Package.swift
@@ -13,12 +13,10 @@ let package = Package(
     traits: [
         .default(enabledTraits: [
             "ProtobufCodable",
-            "UncheckedArraySubscript",
-            "SpecializeScheduleConfiguration"
+            "UncheckedArraySubscript"
         ]),
         .trait(name: "ProtobufCodable"),
-        .trait(name: "UncheckedArraySubscript"),
-        .trait(name: "SpecializeScheduleConfiguration")
+        .trait(name: "UncheckedArraySubscript")
     ],
     dependencies: [
         .package(url: "https://github.com/RandomHashTags/swift-staticdatetime", from: "0.3.5"),

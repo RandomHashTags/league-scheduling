777c384f4a26202ca837d096950674f85e0f3be6
RandomHashTags <imrandomhashtags@gmail.com>
2026-03-20 11:49:05 -0500

    documentation fix

--- Sources/league-scheduling/util/FlippableMatchup.swift
+++ Sources/league-scheduling/util/FlippableMatchup.swift
@@ -1,7 +1,7 @@
 
 /// A scheduled `Matchup` where the home and away teams can be swapped.
 /// 
-/// Only used when balancing the final scheduled matchup's home/away.
+/// Only used when balancing the final scheduled matchups' home/away.
 struct FlippableMatchup: Hashable, Sendable {
     let day:DayIndex
     var matchup:Matchup
